173b30e3cac14699165058992c404b95727bdc6e
Kenichi Takahashi <kenichi.takahashi@kabuku.co.jp>
2019-12-02 21:49:25 +0900

    スワイプでボールを投げる機能を追加

---
+++ ARKitten/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            float x = Input.GetAxis("Mouse X");
+            float y = Input.GetAxis("Mouse Y");
+            transform.Rotate(y, -x, 0);
+        }
+#endif           
+    }
+}
